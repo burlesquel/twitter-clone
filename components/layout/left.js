14faf2573b7ea99bfed5e1d85a8aca15e28b724d
burlesquel <batumanav@gmail.com>
2022-07-30 07:06:56 +0300

    bottom navbar mobile layout

--- components/layout/left.js
+++ components/layout/left.js
@@ -1,54 +1,76 @@
 import Image from "next/image"
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import styles from "./left.module.css"
 import { useRouter } from "next/router"
 import Link from "next/link"
 import ProfileBadge from "../profileBadge"
 import Context from "../../context"
-const LeftItem = ({ icon_name, label, dir }) => {
+const LeftItem = ({ icon_name, label, dir, className }) => {
   const router = useRouter()
   const currentPage = router.asPath.split("/").includes(dir)
   return (
-    <Link href={`/${dir}`}>
-      <div>
-        <i style={{ color: icon_name === "twitter" ? "#1DA1F2" : "" }}
-          className={`bi bi-${icon_name}${(icon_name != "twitter" && icon_name != "hash") && currentPage ? '-fill' : ""}`}></i>
-        {label && <span style={{ fontWeight: currentPage && "bolder" }}>
-          {label}
-        </span>}
-      </div>
-    </Link>
+    <div className={className}>
+      <Link href={`/${dir}`}>
+        <div>
+          <i style={{ color: icon_name === "twitter" ? "#1DA1F2" : ""}}
+            className={`bi bi-${icon_name}${(icon_name != "twitter" && icon_name != "hash") && currentPage ? '-fill' : ""}`}></i>
+          {label && <span style={{ fontWeight: currentPage && "bolder" }}>
+            {label}
+          </span>}
+        </div>
+      </Link>
+    </div>
   )
 }
 
 export default function Left({ className }) {
+
+  // function setDisplayBySize() {
+  //   console.log(window.innerWidth);
+  //   if (window.innerWidth < 500) {
+  //     setDisplay(false)
+  //   }
+  //   else {
+  //     setDisplay(true)
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener("resize", setDisplayBySize)
+  //   return () => {
+  //     window.removeEventListener("resize", setDisplayBySize)
+  //   }
+  // }, [])
+
+  // const [display, setDisplay] = useState(true)
+
   const context = useContext(Context)
   return (
     // width: 26%
     <nav className={className}>
 
       <div className={styles.nav}>
-        <LeftItem icon_name={"twitter"} dir={"home"} />
-        <LeftItem icon_name={"house"} label="Home" dir={"home"} />
-        <LeftItem icon_name={"hash"} label="Explore" dir={"explore"} />
-        <LeftItem icon_name={"bell"} label="Notifications" dir={"notifications"} />
-        <LeftItem icon_name={"envelope"} label="Messages" dir={"messages"} />
-        <LeftItem icon_name={"bookmark"} label="Bookmarks" dir={"bookmarks"} />
-        <LeftItem icon_name={"list"} label="Lists" dir={"lists"} />
-        <LeftItem icon_name={"person"} label="Profile" dir={"profile"} />
-        <LeftItem icon_name={"three-dots"} label="More" dir={"more"} />
-        <div className={styles.tweetButton}>
+        <LeftItem className={` ${styles.twitterBird}`} icon_name={"twitter"} dir={"home"} />
+        <LeftItem className={`${styles.leftItem}`} icon_name={"house"} label="Home" dir={"home"} />
+        <LeftItem className={`${styles.leftItem}`} icon_name={"hash"} label="Explore" dir={"explore"} />
+        <LeftItem className={`${styles.leftItem}`} icon_name={"bell"} label="Notifications" dir={"notifications"} />
+        <LeftItem className={`${styles.leftItem}`} icon_name={"envelope"} label="Messages" dir={"messages"} />
+        <LeftItem className={`${styles.leftItem} ${styles.hideOnMobile}`} icon_name={"bookmark"} label="Bookmarks" dir={"bookmarks"} />
+        <LeftItem className={`${styles.leftItem} ${styles.hideOnMobile}`} icon_name={"list"} label="Lists" dir={"lists"} />
+        <LeftItem className={`${styles.leftItem} ${styles.hideOnMobile}`} icon_name={"person"} label="Profile" dir={"profile"} />
+        <LeftItem className={`${styles.leftItem} ${styles.hideOnMobile}`} icon_name={"three-dots"} label="More" dir={"more"} />
+        <div className={`${styles.tweetButton} ${styles.hideOnMobile}`}>
           <Link href={"#"} >
             Tweet
           </Link>
         </div>
       </div>
 
-      <ProfileBadge onClick={()=>{
+      <ProfileBadge onClick={() => {
         localStorage.clear("id")
         context.setUser(null)
         context.setLoggedIn(false)
-      }} user={context.user} className={styles.profileBadge}/>
+      }} user={context.user} className={`${styles.profileBadge} ${styles.hideOnMobile}`} />
 
     </nav>
   )
--- components/tweet.js
+++ components/tweet.js
@@ -13,27 +13,27 @@ function relativeTime(date_in_ms) {
   var elapsed = new Date().getTime() - date_in_ms;
 
   if (elapsed < msPerMinute) {
-    return Math.round(elapsed / 1000) + ' seconds ago';
+    return Math.round(elapsed / 1000) + 's';
   }
 
   else if (elapsed < msPerHour) {
-    return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    return Math.round(elapsed / msPerMinute) + 'm';
   }
 
   else if (elapsed < msPerDay) {
-    return Math.round(elapsed / msPerHour) + ' hours ago';
+    return Math.round(elapsed / msPerHour) + 'h';
   }
 
   else if (elapsed < msPerMonth) {
-    return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
+    return  Math.round(elapsed / msPerDay) + 'd';
   }
-
+  
   else if (elapsed < msPerYear) {
-    return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
+    return  Math.round(elapsed / msPerMonth) + 'mo';
   }
 
   else {
-    return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
+    return  Math.round(elapsed / msPerYear) + 'y';
   }
 }
 
