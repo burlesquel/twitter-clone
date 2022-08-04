0164b82568174c4ea3af7b7e68871de55b297ed7
burlesquel <batumanav@gmail.com>
2022-08-04 04:12:01 +0300

    infinite list

--- components/tweets.js
+++ components/tweets.js
@@ -4,6 +4,7 @@ import { Server } from '../API'
 import Context from '../context'
 import Tweet from './tweet'
 import { Oval } from 'react-loader-spinner'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 var key = 0
 const keyGenerator = () => {
@@ -11,52 +12,96 @@ const keyGenerator = () => {
     return key
 }
 
-var loadingEffectOnInital = true
+var limit = 20
+var page = 20
 
 export default function Tweets({ query }) {
+
     const router = useRouter()
     const context = useContext(Context)
-    const [tweets, setTweets] = useState(null)
+    const [tweets, setTweets] = useState([])
+    const [hasMore, setHasMore] = useState(true)
+    const [dataLength, setDataLength] = useState(20)
     // YOU CAN CREATE A WAITING STATE
     // console.log("router.pathname: ", router.pathname);
     // console.log("router.aspath: ", router.asPath);
     // console.log("router.basePath: ", router.basePath);
 
-    const refreshTweets = (setLoadingTrue = false) => {
-        setLoadingTrue && setTweets(null)
+    const getTweets = (setLoadingTrue = false, paginated = false) => {
+        if (paginated) {
+            Server.getTweets({ ...query, limit: limit, page: page }).then(res => {
+                if (res.data.length === 0) {
+                    setHasMore(false)
+                    setDataLength(0)
+                }
+                else {
+                    setHasMore(true)
+                    setDataLength(res.data.length)
+                    setTweets(tweets.concat(res.data))
+                }
+
+                page = page + limit
+
+            }).catch(err => {
+                // ERROR ALGHORITM
+                console.log("ERROR WHILE GETTING TWEETS: ", err);
+                setTweets([])
+            })
+
+        }
+        else {
+            Server.getTweets({ ...query, limit: 20, page: 0 }).then(res => {
+                setTweets(res.data)
+            }).catch(err => {
+                // ERROR ALGHORITM
+                console.log("ERROR WHILE GETTING TWEETS: ", err);
+                setTweets([])
+            })
+        }
+        setLoadingTrue && setTweets([])
+
         console.log("refreshing tweets with: ", query);
-        Server.getTweets(query).then(res => {
-            // console.log(Array.from(res.data, tweet=>{console.log(tweet.content.text);}));
-            setTweets(res.data.reverse())
-        }).catch(err => {
-            // ERROR ALGHORITM
-            console.log("ERROR WHILE GETTING TWEETS: ", err);
-            setTweets(null)
-        })
+        console.log("getting tweets with the page of ", page);
     }
 
-    useEffect(() => { refreshTweets(true) }, [])
-    useEffect(() => { refreshTweets(false) }, [router])
+    useEffect(() => { getTweets(true, false) }, [])
+    useEffect(() => { getTweets(false, false) }, [router])
 
 
-    if (tweets) {
+    if (tweets.length !== 0) {
         return (
-            <>
+            <InfiniteScroll
+                dataLength={dataLength}
+                endMessage={<span></span>}
+                hasMore={hasMore}
+                next={() => { getTweets(false, true) }}
+                loader={
+                    <div style={{ display: "flex", justifyContent: "center", padding: "3rem", width: "100%" }}>
+                        <Oval
+                            color='#1DA1F2'
+                            secondaryColor='#74c1f1'
+                            width={"3rem"}
+                            height={"3rem"} />
+                    </div>
+                }
+            >
+
                 {tweets.map(
                     tweet => {
                         if (tweet.retweet) {
                             return (
-                                    <Tweet retweet={true} key={keyGenerator()} tweet_={tweet} refreshTweets={refreshTweets} />
+                                <Tweet retweet={true} key={keyGenerator()} tweet_={tweet} refreshTweets={getTweets} />
                             )
                         }
                         else {
                             return (
-                                    <Tweet tweet_={tweet} key={keyGenerator()} refreshTweets={refreshTweets} />
+                                <Tweet tweet_={tweet} key={keyGenerator()} refreshTweets={getTweets} />
                             )
                         }
                     }
                 )}
-                </>
+
+            </InfiniteScroll>
 
         )
     }
